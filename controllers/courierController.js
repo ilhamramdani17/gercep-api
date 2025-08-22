7fe679173166c10a17a5fbd3043e22ee5a364948
ilhamramdani17 <127287773+ilhamramdani17@users.noreply.github.com>
2025-08-22 22:12:11 +0800

    Create courierController.js

---
+++ controllers/courierController.js
@@ -0,0 +1,46 @@
+import hash from "bcrypt";
+import Courier from "../models/Courier.js";
+
+export const handleCourierLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        code: 400,
+        success: false,
+        message: "Email and password are required",
+      });
+    }
+
+    const courier = await Courier.findOne({ email });
+    if (!courier) {
+      return res.status(404).json({
+        code: 404,
+        success: false,
+        message: "Courier not found",
+      });
+    }
+    const isPasswordValid = await hash.compare(password, courier.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        code: 401,
+        success: false,
+        message: "Invalid password",
+      });
+    }
+    return res.status(200).json({
+      code: 200,
+      success: true,
+      courier: {
+        name: courier.name,
+        photo: courier.photo,
+        email: courier.email,
+        whatsapp: courier.whatsapp,
+      },
+      message: "Courier login successful",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
