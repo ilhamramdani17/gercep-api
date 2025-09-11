be379d114d516e61c87e123af5ac2ac709e14280
ilhamramdani17 <127287773+ilhamramdani17@users.noreply.github.com>
2025-09-12 06:54:28 +0800

    Update courierRouter.js

--- routes/courierRouter.js
+++ routes/courierRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { handleCourierLogin } from "../controllers/courierController.js";
+import { handleCourierLogin,handleCourierStatistik } from "../controllers/courierController.js";
 
 const courierRouter = express.Router();
 
 courierRouter.post("/courier/login", handleCourierLogin);
+courierRouter.get("/get/courier/statistik", handleCourierStatistik);
 
 export default courierRouter;
