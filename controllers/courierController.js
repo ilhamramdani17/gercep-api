2341b4da78b76a785e64400623ce8dae9e437bfd
ilhamramdani17 <127287773+ilhamramdani17@users.noreply.github.com>
2025-09-12 06:56:01 +0800

    Update courierController.js

--- controllers/courierController.js
+++ controllers/courierController.js
@@ -1,5 +1,6 @@
 import hash from "bcrypt";
 import Courier from "../models/Courier.js";
+import Delivery from "../models/Delivery.js";
 
 export const handleCourierLogin = async (req, res) => {
   try {
@@ -41,6 +42,43 @@ export const handleCourierLogin = async (req, res) => {
       },
       message: "Courier login successful",
     });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const handleCourierStatistik = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const data = await Delivery.find({
+      "courier": { $elemMatch: { "courierId": id } },
+      "status": { $elemMatch: { "text": "selesai","available": true } }
+    });
+    let totalPrice = 0
+    let totalTime = 0
+    if (!data) {
+      return res.status(404).json({
+        code: 404,
+        success: false,
+        totalTime,
+        totalPrice,
+        totalFinish: data.length,
+        message: "Data not found",
+      });
+    }else {
+      data.forEach((elem)=> {
+        totalTime = totalTime + elem.time;
+        totalPrice = totalPrice + elem.price;
+      })
+      return res.status(200).json({ 
+        code: 200,
+        success: true,
+        totalTime,
+        totalPrice,
+        totalFinish: data.length,
+        message: "Data found",
+      });
+    }
   } catch (error) {
     return res.status(500).json({ message: "Internal server error" });
   }
