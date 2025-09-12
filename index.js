f5c8a6297f3084efa2d67095a3488867047635d2
ilhamramdani17 <127287773+ilhamramdani17@users.noreply.github.com>
2025-09-12 23:30:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const httpServer = http.createServer(app);
 
-export const io = new Server(httpServer, {
+const io = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
@@ -45,5 +45,8 @@ app.use(deliveryRouter);
 httpServer.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+export default io;
+
 
 
